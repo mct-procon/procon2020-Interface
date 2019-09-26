808d83e3a9b5085684e42ea2821a3ce411c3f39f
Go Suzuki <j1618@matsue-ct.jp>
2019-09-26 18:11:29 +0900

    [IMPROVE]Upgraded to .NET CORE 3.0.

--- GameInterface/GameInterface/Input/InputData.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace GameInterface.Input {
-    public struct InputData {
-        public Point Agent1Delta { get; set; }
-        public Point Agent2Delta { get; set; }
-    }
-}
